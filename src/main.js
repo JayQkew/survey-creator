0f03c9e2c05d2423389fd30b4df5b056218aa1bc
Jay-Lee Shih <rayjebb@gmail.com>
2025-05-12 14:33:10 +0200

    adding fots

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router.js'
 import App from './App.vue'
+import './assets/Erode_Complete/Fonts/WEB/css/erode.css'
 
 createApp(App)
     .use(router)
