2873f7d2359ca9dda78ee7d68a2ac0b0e4260ae2
Jay-Lee Shih <rayjebb@gmail.com>
2025-05-14 16:39:52 +0200

    getting survey data success

--- src/views/OwnSurvey.vue
+++ src/views/OwnSurvey.vue
@@ -3,24 +3,36 @@ import { onMounted, ref } from 'vue'
 import surveyData from '../survey-data.json'
 
 const data = ref(null)
+const loading = ref(true)
+const error = ref(null)
 
 onMounted(() => {
-  data.value = surveyData
-});
+  try {
+    data.value = surveyData
+    console.log('Loaded survey data:', data.value)
+  } catch (err) {
+    error.value = `Failed to load data: ${err.message}`
+    console.error('Error loading survey data:', err)
+  } finally {
+    loading.value = false
+  }
+})
 </script>
 
 <template>
-<header>
+  <header>
     <h1>Owner Survey: {{ $route.params.surveyId }}</h1>
     <p>Where the Owner views data, adds questions, & edits the survey</p>
   </header>
   <main>
     <div v-if="loading">Loading data...</div>
     <div v-else-if="error">Error: {{ error }}</div>
+    <div v-else-if="data">
+      <h2 v-if="data[0]">{{ data[0].title }}</h2>
+      <p>Data: {{ data[0].title }}</p>
+    </div>
     <div v-else>
-      <!-- <h2>{{ data.title }}</h2> -->
-      <p>Data:</p>
-      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+      No data available
     </div>
   </main>
 </template>
