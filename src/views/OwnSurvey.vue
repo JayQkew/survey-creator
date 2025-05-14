ba9bbba54f437096f1d51442a4970cf5b67d487e
Jay-Lee Shih <rayjebb@gmail.com>
2025-05-14 16:51:44 +0200

    finding survey instead fo whole array

--- src/views/OwnSurvey.vue
+++ src/views/OwnSurvey.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
 import surveyData from '../survey-data.json'
+const route = useRoute();
 
 const data = ref(null)
 const loading = ref(true)
 const error = ref(null)
 
+const survey = ref(null)
+
 onMounted(() => {
   try {
-    data.value = surveyData
-    console.log('Loaded survey data:', data.value)
+    survey.value = surveyData.find(x => x.id === route.params.surveyId)
   } catch (err) {
     error.value = `Failed to load data: ${err.message}`
     console.error('Error loading survey data:', err)
@@ -28,11 +31,11 @@ onMounted(() => {
     <div v-if="loading">Loading data...</div>
     <div v-else-if="error">Error: {{ error }}</div>
     <div v-else-if="data">
-      <h2 v-if="data[0]">{{ data[0].title }}</h2>
-      <p>Data: {{ data[0].title }}</p>
+        <h2 v-if="survey">{{ survey.title }}</h2>
+        <p>Data: {{ survey.title }}</p>
     </div>
     <div v-else>
-      No data available
+        No data available
     </div>
   </main>
 </template>
