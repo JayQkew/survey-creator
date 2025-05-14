66004cdff72865675e50d93cbde6faa5da8e6266
Jay-Lee Shih <rayjebb@gmail.com>
2025-05-14 16:25:24 +0200

    adding survey json file

--- src/views/OwnSurvey.vue
+++ src/views/OwnSurvey.vue
@@ -1,9 +1,28 @@
-<script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import surveyData from '../survey-data.json'
+
+const data = ref(null)
+
+onMounted(() => {
+  data.value = surveyData
+});
 </script>
 
 <template>
+<header>
     <h1>Owner Survey: {{ $route.params.surveyId }}</h1>
     <p>Where the Owner views data, adds questions, & edits the survey</p>
+  </header>
+  <main>
+    <div v-if="loading">Loading data...</div>
+    <div v-else-if="error">Error: {{ error }}</div>
+    <div v-else>
+      <!-- <h2>{{ data.title }}</h2> -->
+      <p>Data:</p>
+      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+    </div>
+  </main>
 </template>
 
 <style></style>
