562bd090a2eea4c9e18604852f07435135a42ad0
Jay-Lee Shih <rayjebb@gmail.com>
2025-05-21 11:02:20 +0200

    different views for text

--- src/components/QuestionTypes/Text.vue
+++ src/components/QuestionTypes/Text.vue
@@ -1,8 +1,14 @@
 <script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const isRespondent = computed(() => route.path.includes('respondent'))
+
 </script>
 
 <template>
-    <input type="text">
+    <input v-if="isRespondent" type="text">
+    <p v-else>This is a typed Response</p>
 </template>
 
 <style>
