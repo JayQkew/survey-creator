0396ec468671040de7879ad72df19a1190358939
Jay-Lee Shih <rayjebb@gmail.com>
2025-06-17 11:14:59 +0200

    removing new survey

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
 import About from './views/About.vue'
-import NewSurvey from './views/NewSurvey.vue'
 import OwnSurvey from './views/OwnSurvey.vue'
 import Data from './views/Data.vue'
 import ResSurvey from './views/ResSurvey.vue'
@@ -55,11 +54,6 @@ const routes = [
                                 component: Data,
                             }
                         ]
-                    },
-                    {
-                        path: 'new-survey',
-                        name: 'create-survey',
-                        component: NewSurvey
                     },
                     {
                         path: '',
