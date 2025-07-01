6e2d98152c97a7eb6bbadc519288a5b33c489236
Jay-Lee Shih <rayjebb@gmail.com>
2025-07-01 09:38:48 +0200

    adding a answer model

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -2,9 +2,44 @@ const db = require('../config/database')
 
 class Answer{
     static create(answerData, callback){
-        const { user_id, question_id, survey_id } = answerData
+        const { survey_id, value, created_by, question_id } = answerData
         db.query(
-            'INSERT INTO answer '
+            'INSERT INTO answer (survey_id, value, created_by, updated_by, question_id) VALUES (?, ?, ?, ?, ?)',
+            [survey_id, value, created_by, created_by, question_id],
+            callback
+        )
+    }
+
+    static update(answerData, callback){
+        const { user_id, value, updated_by } = answerData
+        db.query(
+            'UPDATE answer SET value = ?, updated_by = ? WHERE id = ?',
+            [value, updated_by, user_id],
+            callback
+        )
+    }
+
+    static delete(answer_id, callback){
+        db.query(
+            'DELETE FROM answer WHERE id = ?',
+            [answer_id],
+            callback
+        )
+    }
+
+    static deleteByQuestion(question_id, callback){
+        db.query(
+            'DELETE FROM answer WHERE question_id = ?',
+            [question_id],
+            callback
+        )
+    }
+
+    static deleteBySurvey(survey_id, callback){
+        db.query(
+            'DELETE FROM answer WHERE survey_id = ?',
+            [survey_id],
+            callback
         )
     }
 }
