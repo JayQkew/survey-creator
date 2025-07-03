c639dcfa0d0d4f9d22399fa2d517f650fb487ad3
Jay-Lee Shih <rayjebb@gmail.com>
2025-07-03 14:27:50 +0200

    adding response api call

--- server/controllers/responseController.js
+++ server/controllers/responseController.js
@@ -2,8 +2,23 @@ const Response = require('../models/Response')
 
 const submitResponses = async (req, res) => {
     const { responses } = req.body
-    if(!responses) res.status(400).json({error: 'no responses to process'})
-    res.status(200).json({data: responses})
+    if(!responses) return res.status(400).json({error: 'no responses to process'})
+    
+    try{
+        for (const r of responses) {
+            await new Promise((resolve, reject) => {
+                Response.create(r, (err, results) => {
+                    if (err) return reject(err)
+                    resolve(results)
+                })
+            })
+        }
+        return res.status(200).json({message: 'Respons successfully submitted'})
+    } catch (err) {
+        console.error('Error submiting response: ', err)
+        return res.status(500).json({ error: error.message })
+    }
+
 }
 
 module.exports = {
