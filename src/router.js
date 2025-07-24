1a42a575b3bbc28b85fc9393d75c34476f3a0eb5
Jay-Lee Shih <rayjebb@gmail.com>
2025-07-24 09:15:26 +0200

    adding new router edits

--- src/router.js
+++ src/router.js
@@ -7,15 +7,17 @@ import Leaderboard from './views/Leaderboard.vue'
 import ContactUs from './views/ContactUs.vue'
 import AboutUs from './views/AboutUs.vue'
 
+import Dashboard from './views/Dashboard.vue'
+import Home from './views/Home.vue'
 import SurveyEdit from './views/SurveyEdit.vue'
 import SurveyData from './views/SurveyData.vue'
-import Home from './views/Home.vue'
+
 import SurveyResponse from './views/SurveyResponse.vue'
+
 import Error from './views/Error.vue'
 
 import EmptyLayout from './layout/EmptyLayout.vue'
 import PageLayout from './layout/PageLayout.vue'
-import { compile } from 'vue'
 
 const routes = [
     {
@@ -57,6 +59,37 @@ const routes = [
                 name: 'respondent',
                 component: SurveyResponse,
                 props: true
+            }
+        ]
+    },
+    {
+        path: '/user/:id',
+        component: PageLayout,
+        props: true,
+        children: [
+            {
+                path: 'dashboard',
+                name: 'dashboard',
+                component: Dashboard
+            },
+            {
+                path: 'surveys',
+                name: 'surveys',
+                component: Home
+            },
+            {
+                path: 'survey/:surveyId',
+                name: 'survey-edit',
+                component: SurveyEdit
+            },
+            {
+                path: 'survey/:surveyId/data',
+                name: 'survey-data',
+                component: SurveyData
+            },
+            {
+                path: '',
+                redirect: { name: 'dashboard'}
             }
         ]
     },
